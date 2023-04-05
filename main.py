12f79eef93d0a95ea247ed60bbabe6a43246d511
jx06T <119775148+jx06T@users.noreply.github.com>
2023-04-05 20:30:53 +0800

    modified:   __pycache__/main.cpython-311.pyc 	modified:   main.py 	modified:   requirements.txt

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI, Request
 import os
 from dotenv import load_dotenv
 import time
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage   
+import json
 
 load_dotenv()
 
@@ -12,46 +15,21 @@ app = FastAPI()
 
 @app.post("/api")
 async def MainApi(request: Request):
-    data = await request.json()
-    print(data)
+    json_data = await request.json()
+    body = await request.body()
+    print(json_data)
     try:
-        return data[0].message.text
+        line_bot_api = LineBotApi('1660832705')
+        handler = WebhookHandler('57d05fcee83d08744d3c34da8a970fc6')
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']         # 取得 reply token
+        msg = json_data['events'][0]['message']['text']   # 取得使用者發送的訊息
+        text_message = TextSendMessage(text=msg)          # 設定回傳同樣的訊息
+        line_bot_api.reply_message(tk,text_message)       # 回傳訊息
     except:
-        pass
-    data = {
-        "type": "text",
-        "text": "Select your favorite food category or send me your location!",
-        "quickReply": {
-            "items": [
-                {
-                    "type": "action",
-                    "imageUrl": "https://example.com/sushi.png",
-                    "action": {
-                        "type": "message",
-                        "label": "Sushi",
-                        "text": "Sushi"
-                    }
-                },
-                {
-                    "type": "action",
-                    "imageUrl": "https://example.com/tempura.png",
-                    "action": {
-                        "type": "message",
-                        "label": "Tempura",
-                        "text": "Tempura"
-                    }
-                },
-                {
-                    "type": "action",
-                    "action": {
-                        "type": "location",
-                        "label": "Send location"
-                    }
-                }
-            ]
-        }
-    }
-    return data
+        print('error')
+    return 'OK'
 
 
 @app.get("/test")
